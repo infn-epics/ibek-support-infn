950fa4c24582118628797a491fbd95a435e24025
aduffizi <alessandro.duffizi@lnf.infn.it>
2024-11-14 11:15:06 +0100

    TPG366 ibek support

---
+++ TPG366/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+##########################################################################
+##### install script for TPG366 support modules ############################
+##########################################################################
+
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=TPG366
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org https://oauth2:zt_ALPjGqNRwLPeHMB8_@baltig.infn.it/lnf-da-control/
+
+
+ibek support register ${NAME}
+
+# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+ibek support add-libs asyn modbus stream
+ibek support add-dbds modbusSupport.dbd asyn.dbd stream.dbd
+
+
+# global config settings
+${FOLDER}/../_global/install.sh ${NAME}
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
+
+
