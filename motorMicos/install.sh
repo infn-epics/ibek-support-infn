d52fd9b66d7f50a8babad45974adba5d82064510
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2026-05-27 08:58:57 +0200

    update micos

--- motorMicos/install.sh
+++ motorMicos/install.sh
@@ -3,24 +3,29 @@
 # ARGUMENTS:
 #  $1 VERSION to install (must match repo tag)
 VERSION=${1}
-NAME=motorNewport
+NAME=motorMicos
 FOLDER=$(dirname $(readlink -f $0))
 
 # log output and abort on failure
 set -xe
 
 # get the source and fix up the configure/RELEASE files
-ibek support git-clone ${NAME} ${VERSION} --org http://github.com/epics-motor/
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/epics-motor/
 ibek support register ${NAME}
 
 # declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
-ibek support add-libs Newport motor asyn
-ibek support add-dbds devNewport.dbd motorRecord.dbd motorSupport.dbd
+ibek support add-libs Micos motor asyn
+ibek support add-dbds devMicos.dbd motorRecord.dbd motorSupport.dbd
 # global config settings
 ${FOLDER}/../_global/install.sh ${NAME}
 
 # compile the support module
 ibek support compile ${NAME}
+
+# Install default Pollux configuration files into module config directory.
+mkdir -p /epics/support/${NAME}/config
+cp -f ${FOLDER}/config/* /epics/support/${NAME}/config/
+
 # prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
 ibek support generate-links ${FOLDER}
 
